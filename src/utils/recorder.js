b29f4e562b2e41a4c5044c349346b3e3b8eea727
明扬 <my@famer.me>
2023-11-24 11:25:51 +0800

    format code

--- src/utils/recorder.js
+++ src/utils/recorder.js
@@ -1,45 +1,45 @@
 import { Subject } from 'rxjs'
 
 const constraints = {
-  audio: true,
-  video: false,
+	audio: true,
+	video: false,
 };
 
 export async function createRecorder() {
-  const subject = new Subject();
+	const subject = new Subject();
 	let context = new AudioContext({
-	    // if Non-interactive, use 'playback' or 'balanced' // https://developer.mozilla.org/en-US/docs/Web/API/AudioContextLatencyCategory
-	    latencyHint: 'interactive',
+		// if Non-interactive, use 'playback' or 'balanced' // https://developer.mozilla.org/en-US/docs/Web/API/AudioContextLatencyCategory
+		latencyHint: 'interactive',
 	});
-    
+
 	await context.audioWorklet.addModule('recorderWorkletProcessor.js')
 	context.resume();
-    
+
 	const globalStream = await navigator.mediaDevices.getUserMedia(constraints)
 	let input = context.createMediaStreamSource(globalStream)
 	let processor = new window.AudioWorkletNode(
-	    context,
-	    'recorder.worklet'
+		context,
+		'recorder.worklet'
 	);
 	processor.connect(context.destination);
 	context.resume()
 	input.connect(processor)
 	processor.port.onmessage = (e) => {
-	    const audioData = e.data;
-      subject.next(audioData)
+		const audioData = e.data;
+		subject.next(audioData)
 	}
-  subject.subscribe({
-    complete() {
-      globalStream.getTracks()[0].stop();
-      input.disconnect(processor);
-      processor.disconnect(context.destination);
-      context.close().then(function () {
-          input = null;
-          processor = null;
-          context = null;
-      });
-      console.log('Recording completed')
-    }
-  })
-  return subject
+	subject.subscribe({
+		complete() {
+			globalStream.getTracks()[0].stop();
+			input.disconnect(processor);
+			processor.disconnect(context.destination);
+			context.close().then(function () {
+				input = null;
+				processor = null;
+				context = null;
+			});
+			console.log('Recording completed')
+		}
+	})
+	return subject
 }
